229120cd419c0de8b34fc3445769eb9d3695b9f6
Elaine5 <lanne9873@gmail.com>
2022-06-28 16:23:27 -0300

    Switch e Temporizador

--- React-JavaScript/script.js
+++ React-JavaScript/script.js
@@ -44,19 +44,46 @@ while(x < 10){
 document.write("</br></br></br> O X esta valendo: "+ x + "</br></br>");
 
 
-
 // For = Para (pode declarar mais de uma condicao)
 
 for(a = 0; a < 10; a++ ){
     document.write("</br> O valor do A: " + a);
-} */
-
+}
 
 // Switch
 
-x = 2;
+document.write("</br> Escolha seu pedido: </br>");
+document.write("</br> 0 - Sorvete / 1 - Suco </br>");
+document.write("</br> 2 - Coca cola / 3 - Agua gelada </br></br>");
 
-switch(x){
-    case 0:
-        alert("O x vale 0!")
+function pedir(){
+
+    x = prompt("O que deseja pedir?");
+
+    switch(x){
+        case "0":
+            alert("Voce pediu Sorvete");
+            break;
+        case "1":
+            alert("Voce pediu Suco");
+            break;
+        case "2":
+            alert("Voce pediu Coca cola");
+            break;
+        case "3":
+            alert("Voce pediu Agua gelada");
+            break;
+        default:
+            alert("Ops nao temos essa opcao");
+            break;    
+    }
+} */
+
+function acao(){
+    document.write("Executando...<br/>");
 }
+
+// Executa de tempo em tempo!
+// setInterval(acao, 1000);
+
+setTimeout(acao, 3000);
